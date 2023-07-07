4471774f886843454ead9091b0b2d57d03981ab8
Youssef <sa.youssef@example.com>
2023-07-07 23:13:06 +0100

    Initial commit

---
+++ src/main/java/org/springmvc/mscsodaservice/MsscSodaServiceApplication.java
@@ -0,0 +1,13 @@
+package org.springmvc.mscsodaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSodaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscSodaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springmvc/mscsodaservice/MsscSodaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springmvc.mscsodaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSodaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
